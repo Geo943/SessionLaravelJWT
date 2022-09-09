6fffb8fa0236e586fff48fee12a783aa70908b5a
Geo943 <ghernandezf1@miumg.edu.gt>
2022-09-08 22:51:25 -0600

    autenticacion con jwt

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,10 +5,12 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+//use Illuminate\Foundation\Auth\User;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class usuario extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -19,10 +21,14 @@ class usuario extends Authenticatable
      */
     protected $fillable = [
         //'id_usuario',
-        'usuario',
+        /*'usuario',
         'id_tipo_usuario',
         'id_empleado',
-        'contrasena'
+        'contrasena'*/
+        'name',
+        'email',
+        'password',
+
     ];
 
     /**
@@ -43,4 +49,27 @@ class usuario extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /////////////////////agregados para jwt
+     // Rest omitted for brevity
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ClieneteController;
@@ -15,12 +16,23 @@ use App\Http\Controllers\ClieneteController;
 |*/
 
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::group([
+    'prefix'=> 'auth'
+],function() {
+
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    //producto.
+    Route::get('/producto', 'App\Http\Controllers\productoController@index');
+    
+}
+
+);
 
 
-Route::get('/producto', 'App\Http\Controllers\productoController@index');
+
+
 
 
 Route::get('/cliente', 'App\Http\Controllers\ClientesController@index');
